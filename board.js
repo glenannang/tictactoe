90b3a81dd3c419933bbda07f486b02810979908d
glenannang <angelaannang8@gmail.com>
2026-08-18 16:49:56 +0800

    refactor: generate game board dynamically with JavaScript

--- board.js
+++ board.js
@@ -3,8 +3,32 @@ function board() {
 
 
 syncBoard(gameKey); //gameKey is a global variable for now 
-const cells = document.querySelectorAll(".cell");
 
+
+const board = document.createElement("div");
+board.id = "board";
+board.className = "board";
+
+for (let y = 0; y < 3; y++) {
+
+    for (let x = 0; x < 3; x++) {
+
+        const cell = document.createElement("button");
+
+        cell.className = "cell";
+        cell.dataset.x = x;
+        cell.dataset.y = y;
+
+        board.appendChild(cell);
+    }
+}
+
+document.body.appendChild(board);
+
+
+
+// Event Listener for each cell
+const cells = document.querySelectorAll(".cell");
 cells.forEach(function (cell) {
 
     cell.addEventListener("click", function () {
